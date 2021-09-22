349e1bd059c0e23c0960e57a5a042a8a507fcb1f
pony-ai <1663794971@qq.com>
2021-09-22 11:34:33 +0800

    upload file.

---
+++ 1.4/dimension.py
@@ -0,0 +1,9 @@
+# dimensions = (100,2)
+# dimensions = (100,0)
+# for dimension in dimensions:
+# 	print(dimension)
+
+foods = ('apple','orange','tomato','pat','meet')
+foods = ('apple','orange','beer','pear','meet')
+for food in foods:
+	print(food)
