b02fc079d7aa8922bcd5d30c3f89330eec65bddf
pony-ai <1663794971@qq.com>
2021-09-23 16:46:49 +0800

    upload file if.py

---
+++ 1.5/if.py
@@ -0,0 +1,36 @@
+# age = 24
+# if age > 24:
+# 	print('you are old.')
+# else:
+# 	print('you are young')
+# alien's color
+# alien_color = ['green','yellow','red']
+# if 'green' in alien_color:
+# 	print('you get 5 points')
+
+# alien's color 2
+# alien = 'yellow'
+# if alien == 'green':
+# 	print('you get 5 points')
+# else:
+# 	print('nothing')
+
+# alien's color 2
+# alien = 'yellow'
+# alien = 'red'
+# alien = 'green'
+# if alien == 'green':  #[Finished in 151ms]
+# 	print('you get 5 points')
+# elif alien == 'yellow':  #[Finished in 152ms]
+# 	print('you get 10 points')
+# elif alien == 'red':  #[Finished in 153ms]
+# 	print('you get 20 point')
+
+# 检查特殊元素
+ocean = ['richal','rose','doudou','nannan']
+for one in ocean:
+	if one == 'doudou':
+		print(f'{one} yes')
+	else:
+		print(f"{one} no")
+print('dump over.')
